6fe4f81da1696f7d7c7579741a4af7801c1d1ecf
harrymt <harrymt@me.com>
2019-07-05 16:40:03 +0100

    Added default public parameters auto generated properties in typescript

--- api/trades/Trade.ts
+++ api/trades/Trade.ts
@@ -3,7 +3,7 @@ import { BuyOrSell } from "./BuyOrSell";
 let nextId = 0;
 
 export class Trade {
-    constructor(type: BuyOrSell, amount: number, price: number) {
+    constructor(public type: BuyOrSell, public amount: number, public price: number) {
         this.type = type;
         this.amount = amount;
         this.price = price;
@@ -13,9 +13,6 @@ export class Trade {
         const t = body as Trade;
         return new this(t.type, t.amount, t.price);
     }
-    type: BuyOrSell;
-    amount: number;
-    price: number;
     id: number;
 
     toJson() {
